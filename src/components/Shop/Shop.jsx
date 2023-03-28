bd7ef93598e55264a77fd1e91775c2aeb2b50b55
ShezanMahmud <shezanmahmud650@gmail.com>
2023-03-28 12:50:38 +0600

    added total p]rice and select items done

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
@@ -7,7 +8,9 @@ const Shop = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    fetch("products.json")
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
@@ -30,8 +33,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="card-container">
-        <h4>Order Summary</h4>
-        <p>Selected Items: {cart.length}</p>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
