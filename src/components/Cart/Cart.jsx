5579aa607b739d9e79bb9ae9fdf8a9d09f6ebd8a
ShezanMahmud <shezanmahmud650@gmail.com>
2023-04-08 18:10:53 +0600

    added clear btn and functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import "./Cart.css";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleClearCart }) => {
   // console.log(cart);
 
   let totalPrice = 0;
@@ -28,6 +30,10 @@ const Cart = ({ cart }) => {
       <p>Total Shipping Charge: {totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
       <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+      <button onClick={handleClearCart} className="btn-clear-cart">
+        <span>Clear Cart: </span>
+        <FontAwesomeIcon icon={faTrashAlt} />
+        </button>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { addToDb, getShoppingCart } from "../../utilities/fakedb";
+import { addToDb, deleteShoppingCart, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -54,6 +54,11 @@ const Shop = () => {
     addToDb(product.id);
   };
 
+  const handleClearCart = () => {
+    setCart([])
+    deleteShoppingCart()
+  }
+
   return (
     <div className="shop-container">
       <div className="products-container">
@@ -66,7 +71,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="card-container">
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart} handleClearCart={handleClearCart}></Cart>
       </div>
     </div>
   );
